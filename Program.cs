158b85fe865a31bbd865c21825aa2883326a108b
loukeeehh <louka.constant@gmail.com>
2024-03-25 15:14:51 +0100

    Mon application de matrices

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿namespace _5T24_LoukaConstant_appliMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[,] Matrice1;
+            int[,] Matrice2;
+            int[,] Matrice3;
+            int[,] MatriceMulti;
+            bool addition;
+            int lignes;
+            int colonnes;
+            int nUser;
+            string chaineResultat;
+            string repeatProg;
+            appliMatrices mesMatrices = new appliMatrices();
+
+            do
+            {
+                //première matrice
+                Console.WriteLine("Combien de lignes voulez-vous à votre matrices ?");
+                lignes = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Combien de colonnes voulez-vous à votre matrice ?");
+                colonnes = int.Parse(Console.ReadLine());
+
+                //traitement première matrice 
+                mesMatrices.genereMatrice(lignes, colonnes, out Matrice1);
+
+                //affichage première matrice
+                Console.WriteLine("Voici votre première matrice :");
+                mesMatrices.concateneMatrice(Matrice1, out chaineResultat);
+                Console.WriteLine(chaineResultat);
+
+                //deuxième matrice
+                Console.WriteLine("Combien de lignes voulez-vous à votre deuxième matrice ?");
+                lignes = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Combien de colonnes voulez-vous à votre deuxième matrice ?");
+                colonnes = int.Parse(Console.ReadLine());
+
+                //traitement deuxième matrice
+                mesMatrices.genereMatrice(lignes, colonnes, out Matrice2);
+
+                //affichage deuxième matrice
+                Console.WriteLine("Voici votre deuxieme matrice :");
+                mesMatrices.concateneMatrice(Matrice2, out chaineResultat);
+                Console.WriteLine(chaineResultat);
+
+
+                //demande d'opération à faire 
+                Console.WriteLine("Quelle opération voulez-vous réaliser à vos deux matrices ? Taper 1 pour l'addition et taper 2 pour la multiplication");
+                nUser = int.Parse(Console.ReadLine());
+
+                //opération addition
+                if (nUser == 1)
+                {
+                    Console.WriteLine("Voici l'opération de l'addition :\n");
+                    mesMatrices.concateneMatrice(Matrice1, out chaineResultat);
+                    Console.WriteLine($"{chaineResultat}\n");
+                    Console.WriteLine("+\n");
+                    mesMatrices.concateneMatrice(Matrice2, out chaineResultat);
+                    Console.WriteLine($"{chaineResultat}\n");
+                    Console.WriteLine("=\n");
+                    mesMatrices.additionMatrice(Matrice1, Matrice2, out Matrice3, out addition);
+                    if (addition == true)
+                    {
+                        mesMatrices.concateneMatrice(Matrice3, out chaineResultat);
+                        Console.WriteLine(chaineResultat);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Vous ne pouvez pas faire cette opération !");
+                    }
+
+                }
+
+                //opération multiplication
+                else if (nUser == 2)
+                {
+                    Console.WriteLine("Voici l'opération de la multiplication :\n");
+                    mesMatrices.concateneMatrice(Matrice1, out chaineResultat);
+                    Console.WriteLine($"{chaineResultat}\n");
+                    Console.WriteLine("*\n");
+                    mesMatrices.concateneMatrice(Matrice2, out chaineResultat);
+                    Console.WriteLine($"{chaineResultat}\n");
+                    Console.WriteLine("=\n");
+                    mesMatrices.multiplicationMatrice(Matrice1, Matrice2, out MatriceMulti, out addition);
+                    if (addition == true)
+                    {
+                        mesMatrices.concateneMatrice(MatriceMulti, out chaineResultat);
+                        Console.WriteLine(chaineResultat);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Vous ne pouvez pas faire cette opération !");
+                    }
+                }
+
+                //demande de reprise                
+                Console.WriteLine("Voulez-vous recommencer ? 'o' = oui, 'n' = non.");
+                repeatProg = Console.ReadLine();
+
+            } while (repeatProg == "o");
+        }
+    }
+}
+
---
+++ mesMatrices.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5T24_LoukaConstant_appliMatrices
+{
+    public struct appliMatrices
+    {
+        public void genereMatrice(int lignes, int colonnes, out int[,] Matrice)
+
+        {
+            Matrice = new int[lignes, colonnes];
+            Random alea = new Random();
+
+            for (lignes = 0; lignes <= Matrice.GetLength(0) - 1; lignes++)
+            {
+                for (colonnes = 0; colonnes <= Matrice.GetLength(1) - 1; colonnes++)
+                {
+                    Matrice[lignes, colonnes] = alea.Next(0, 10);
+                }
+            }
+
+        }
+
+        public void concateneMatrice(int[,] Matrice, out string concatene)
+        {
+            concatene = "";
+
+            for (int i = 0; i <= Matrice.GetLength(0) - 1; i++)
+            {
+                string lignes = "";
+
+                for (int j = 0; j <= Matrice.GetLength(1) - 1; j++)
+                {
+                    lignes = lignes + Matrice[i, j];
+
+                    if (j != Matrice.GetLength(1) - 1)
+                    {
+                        lignes = lignes + ",";
+                    }
+                }
+                concatene = concatene + "\n" + lignes;
+            }
+        }
+        public void additionMatrice(int[,] Matrice1, int[,] Matrice2, out int[,] Matrice3, out bool addition)
+        {
+            addition = true;
+            int lignes;
+            int colonnes;
+            Matrice3 = new int[Matrice1.GetLength(0), Matrice1.GetLength(1)];
+
+            if (Matrice1.GetLength(0) == Matrice2.GetLength(0) && Matrice1.GetLength(1) == Matrice2.GetLength(1))
+            {
+
+                for (lignes = 0; lignes <= Matrice1.GetLength(0) - 1; lignes++)
+                {
+                    for (colonnes = 0; colonnes <= Matrice2.GetLength(0) - 1; colonnes++)
+                    {
+                        Matrice3[lignes, colonnes] = Matrice1[lignes, colonnes] + Matrice2[lignes, colonnes];
+                    }
+                }
+
+            }
+
+            else
+            {
+                addition = false;
+
+            }
+
+        }
+
+        public void multiplicationMatrice(int[,] Matrice1, int[,] Matrice2, out int[,] MatriceMulti, out bool addition)
+        {
+            if (Matrice1.GetLength(1) == Matrice2.GetLength(0))
+            {
+                addition = true;
+                int lignes;
+                int colonnes;
+                int i;
+                MatriceMulti = new int[Matrice1.GetLength(0), Matrice2.GetLength(1)];
+
+                for (lignes = 0; lignes <= Matrice1.GetLength(0) - 1; lignes++)
+                {
+                    for (colonnes = 0; colonnes <= Matrice2.GetLength(1) - 1; colonnes++)
+                    {
+                        MatriceMulti[lignes, colonnes] = 0;
+
+                        for (i = 0; i <= Matrice1.GetLength(1) - 1; i++)
+                        {
+                            MatriceMulti[lignes, colonnes] += Matrice1[lignes, i] * Matrice2[i, colonnes];
+                        }
+
+                    }
+
+                }
+            }
+
+            else
+            {
+                addition = false;
+                MatriceMulti = null;
+            }
+
+
+        }
+    }
+}
